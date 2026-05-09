4a51c110f109e4a690c59db5e27c6978c29ff9ba
Najuna Brian <najuna.dev@gmail.com>
2026-05-09 13:19:19 +0300

    Configure Supabase project and initialize database schema.
    Link the app to the new themugerwas Supabase instance and add migration assets so RSVP tables and policies can be pushed consistently.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WEDDING_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ihaneexhqmrhdbjtcmdc.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloYW5lZXhocW1yaGRianRjbWRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMDYzMjYsImV4cCI6MjA5Mzg4MjMyNn0.ME_WRqtgXoXqhy7MJe28WWLT4mATVemVwEhzHpqp5iw",
   rsvpDeadlineIso: "2026-08-01T23:59:59+03:00",
   expectedGuestTotal: 200,
   siteBaseUrl: window.location.origin,
